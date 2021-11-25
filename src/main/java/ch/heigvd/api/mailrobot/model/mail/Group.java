0b517ea439663270dc5bf9ad5c54497acc746be9
Stefano <stefano.pontarolo@heig-vd.ch>
2021-11-25 10:13:29 +0100

    Complete class group

--- src/main/java/ch/heigvd/api/mailrobot/model/mail/Group.java
+++ src/main/java/ch/heigvd/api/mailrobot/model/mail/Group.java
@@ -5,5 +5,18 @@ package main.java.ch.heigvd.api.mailrobot.model.mail;
             Il doit être possible d'ajouter des personnes
  */
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Group {
+
+    private final List<Person> members = new ArrayList<>();
+
+    public void addMember(Person person){
+        members.add(person);
+    }
+
+    public List<Person> getMembers() {
+        return new ArrayList<>(members);
+    }
 }
