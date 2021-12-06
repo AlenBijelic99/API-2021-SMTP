1c73976e80c2ecf2349dcabb99e4bfe44b318f92
Stefano <stefano.pontarolo@heig-vd.ch>
2021-12-06 12:44:46 +0100

    first version of mailRobot

--- src/main/java/ch/heigvd/api/mailrobot/MailRobot.java
+++ src/main/java/ch/heigvd/api/mailrobot/MailRobot.java
@@ -1,7 +1,12 @@
 package main.java.ch.heigvd.api.mailrobot;
 
+import main.java.ch.heigvd.api.mailrobot.config.ConfigManager;
+import main.java.ch.heigvd.api.mailrobot.model.prank.PrankGenerator;
+
 public class MailRobot {
     public static void main(String[] args) {
-        // Launch prank generator
+        ConfigManager configManager = new ConfigManager();
+        PrankGenerator prankGenerator = new PrankGenerator(configManager);
+        prankGenerator.generatePranks();
     }
 }
